6d5e989952a9abe4345e53fd3510b54ab9c98655
erdemtoruk <erdemtoruk26@gmail.com>
2025-02-20 14:32:54 +0300

    Comments added

--- include/exam.hpp
+++ include/exam.hpp
@@ -13,13 +13,27 @@ class exam{
     public:
         exam(string _name, string _date, float _percentage, float _score);
 
+        //Returns the name of the exam
         string get_name();
+
+        //Returns the date of the exam 
         string get_date();
+
+        //Returns the percentage of the exam
         float get_percentage();
+
+        // Returns the score of the exam
         float get_score();
 
+        //Sets the name of the exam
         void change_name(string new_name);
+
+        //Sets the date of the exam
         void change_date(string new_date);
+        
+        //Sets the percentage of the exam
         void change_percentage(int new_percentage);
+
+        //Sets the score of the exam
         void change_score(int new_score);
 };
